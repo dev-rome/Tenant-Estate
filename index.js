52e63573032293e0047a44aafa795226a1411bb5
Jerome Haynes <info.devrome@gmail.com>
2022-04-16 19:23:42 -0400

    set up index.js file

---
+++ index.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const path = require("path");
+const expressLayouts = require("express-ejs-layouts");
+const methodOverride = require("method-override");
+const homeController = require("./controller/homeController");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.set("port", process.env.PORT || 4000);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(expressLayouts);
+app.use(express.static(__dirname + "/public"));
+
+app.use("/buy", homeController);
+
+app.listen(app.get("port"), () => {
+  console.log(`Server running on Port: ${app.get("port")}`);
+});
