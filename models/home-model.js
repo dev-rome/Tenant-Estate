b106e2f338441cc0229bcc95083d942fe3fb507d
Jerome Haynes <info.devrome@gmail.com>
2022-04-19 10:21:39 -0400

    added required message

--- models/home-model.js
+++ models/home-model.js
@@ -4,39 +4,39 @@ const Schema = mongoose.Schema;
 const HomeSchema = new Schema({
   image: {
     type: String,
-    required: true,
+    required: [true, "Please provide an image"],
   },
   price: {
     type: Number,
-    required: true,
+    required: [true, "Please provide a price"],
   },
   name: {
     type: String,
-    required: true,
+    required: [true, "Please provide a name for the home"],
   },
   description: {
     type: String,
-    required: true,
+    required: [true, "Home description is required"],
   },
   location: {
     type: String,
-    required: true,
+    required: [true, "Home location is required"],
   },
   bedrooms: {
     type: Number,
-    required: true,
+    required: [true, "How many bedrooms?"],
   },
   bathrooms: {
     type: Number,
-    required: true,
+    required: [true, "How many bathrooms?"],
   },
   sqft: {
     type: String,
-    required: true,
+    required: [true, "How many square feet?"],
   },
   properType: {
     type: String,
-    required: true,
+    required: [true, "Please provide a proper type"],
   }
 });
 
