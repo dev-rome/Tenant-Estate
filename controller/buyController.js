3f53657450f4e72dc6d24294180b2f8e5c01a445
Jerome Haynes <info.devrome@gmail.com>
2022-04-19 21:18:36 -0400

    added more routes to buy and rent controller

--- controller/buyController.js
+++ controller/buyController.js
@@ -8,11 +8,39 @@ router.get("/", async (req, res) => {
   res.render("buy/buy", { homes });
 });
 
+router.get("/new", (req, res) => {
+  res.render("buy/newHome");
+});
+
+router.post("/", async (req, res) => {
+  const home = new Home(req.body.home);
+  await home.save();
+  res.redirect(`buy/${home._id}`);
+});
+
 router.get("/:id", async (req, res) => {
   const id = req.params.id;
   const home = await Home.findById(id);
   res.render("buy/buyDetails", { home });
-})
+});
+
+router.get("/:id/edit", async (req, res) => {
+  const id = req.params.id;
+  const home = await Home.findById(id);
+  res.render("buy/editHome", { home });
+});
+
+router.put("/:id", async (req, res) => {
+  const id = req.params.id;
+  const home = await Home.findByIdAndUpdate(id, req.body.home);
+  res.redirect(`buy/${home._id}`);
+});
+
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  await Home.findByIdAndDelete(id);
+  res.redirect("/buy");
+});
 
 // router.get("/search", async (req, res) => {
 //   const { location, price, properType } = req.query;
--- controller/rentController.js
+++ controller/rentController.js
@@ -4,14 +4,42 @@ const router = express.Router();
 const Apartment = require("../models/apartment-model");
 
 router.get("/", async (req, res) => {
-    const homes = await Apartment.find({});
-    res.render("rent/rent", { homes });
-  });
-  
-  router.get("/:id", async (req, res) => {
-    const id = req.params.id;
-    const home = await Apartment.findById(id);
-    res.render("rent/rentDetails", { home });
-  })
+  const apartments = await Apartment.find({});
+  res.render("rent/rent", { apartments });
+});
+
+router.get("/new", (req, res) => {
+  res.render("rent/new");
+});
+
+router.post("/", async (req, res) => {
+  const apartment = new Apartment(req.body.apartment);
+  await apartment.save();
+  res.redirect(`rent/${apartment._id}`);
+});
+
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  const apartment = await Apartment.findById(id);
+  res.render("rent/rentDetails", { apartment });
+});
+
+router.get("/:id/edit", async (req, res) => {
+  const id = req.params.id;
+  const apartment = await Apartment.findById(id);
+  res.render("rent/edit", { apartment });
+});
+
+router.put("/:id", async (req, res) => {
+  const id = req.params.id;
+  const apartment = await Apartment.findByIdAndUpdate(id, req.body.apartment);
+  res.redirect(`rent/${apartment._id}`);
+});
+
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  await Apartment.findByIdAndDelete(id);
+  res.redirect("/rent");
+});
 
 module.exports = router;
