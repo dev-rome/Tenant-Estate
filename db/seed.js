1d5c4b351942e4db5ed749e4745228c28d7e58c1
Jerome Haynes <info.devrome@gmail.com>
2022-04-19 21:18:03 -0400

    added apartment data

--- db/seed.js
+++ db/seed.js
@@ -1,11 +1,24 @@
 require("dotenv").config();
 
-const Home = require("../models/home-model");
-const seedData = require("./home.json");
+// const Home = require("../models/home-model");
+// const homeData = require("./home.json");
+
+const Apartment = require("../models/apartment-model");
+const apartmentData = require("./apartment.json");
 
 Home.deleteMany({})
   .then(() => {
-    return Home.insertMany(seedData);
+    return Home.insertMany(homeData);
+  })
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => {
+    process.exit();
+  });
+
+Apartment.deleteMany({})
+  .then(() => {
+    return Apartment.insertMany(apartmentData);
   })
   .then(console.log)
   .catch(console.error)
