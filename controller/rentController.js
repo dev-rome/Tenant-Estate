54c2cc77ceca45f041fbefc9142cc48ab81ee548
Jerome Haynes <info.devrome@gmail.com>
2022-04-23 13:48:09 -0400

    added flash errors, removed review routes, check if user is logged in

--- controller/rentController.js
+++ controller/rentController.js
@@ -3,61 +3,56 @@ const router = express.Router();
 
 const Apartment = require("../models/apartment-model");
 const Review = require("../models/review-model");
+const { isLoggedIn } = require("../middleware/isLogged");
 
 router.get("/", async (req, res) => {
   const apartments = await Apartment.find({});
   res.render("rent/rent", { apartments });
 });
 
-router.get("/new", (req, res) => {
+router.get("/new", isLoggedIn, (req, res) => {
   res.render("rent/newApartment");
 });
 
-router.post("/", async (req, res) => {
+router.post("/", isLoggedIn, async (req, res) => {
   const apartment = new Apartment(req.body.apartment);
   await apartment.save();
+  req.flash("success", "Apartment added successfully!");
   res.redirect(`rent/${apartment._id}`);
-});
-
-router.post("/:id/reviews", async (req, res) => {
-  const id = req.params.id;
-  const apartment = await Apartment.findById(id);
-  const review = new Review(req.body.review);
-  apartment.reviews.push(review);
-  await apartment.save();
-  await review.save();
-  res.redirect(`/rent/${apartment._id}`);
 });
 
 router.get("/:id", async (req, res) => {
   const id = req.params.id;
   const apartment = await Apartment.findById(id).populate("reviews");
+  if (!apartment) {
+    req.flash("error", "Home not found!");
+    return res.redirect("/rent");
+  }
   res.render("rent/rentDetails", { apartment });
 });
 
-router.get("/:id/edit", async (req, res) => {
+router.get("/:id/edit", isLoggedIn, async (req, res) => {
   const id = req.params.id;
   const apartment = await Apartment.findById(id);
+  if (!apartment) {
+    req.flash("error", "Home not found!");
+    return res.redirect("/rent");
+  }
   res.render("rent/editApartment", { apartment });
 });
 
-router.put("/:id", async (req, res) => {
+router.put("/:id", isLoggedIn, async (req, res) => {
   const id = req.params.id;
   const apartment = await Apartment.findByIdAndUpdate(id, req.body.apartment);
-  res.redirect(`rent/${apartment._id}`);
+  req.flash("success", "Successfully updated apartment!");
+  res.redirect(`/rent/${apartment._id}`);
 });
 
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", isLoggedIn, async (req, res) => {
   const id = req.params.id;
   await Apartment.findByIdAndDelete(id);
+  req.flash("success", "Apartment deleted successfully!");
   res.redirect("/rent");
-});
-
-router.delete("/:id/reviews/:reviewId", async (req, res) => {
-  const { id, reviewId } = req.params;
-  await Apartment.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
-  await Review.findByIdAndDelete(reviewId);
-  res.redirect(`/rent/${id}`);
 });
 
 module.exports = router;
