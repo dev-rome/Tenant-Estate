dc1dcedea1d763f3a7c4e8daf639f81c3b7f8781
Jerome Haynes <info.devrome@gmail.com>
2022-04-23 13:47:37 -0400

    added flash errors, removed review routes, check if user is logged in

--- controller/buyController.js
+++ controller/buyController.js
@@ -2,62 +2,56 @@ const express = require("express");
 const router = express.Router();
 
 const Home = require("../models/home-model");
-const Review = require("../models/review-model");
+const { isLoggedIn } = require("../middleware/isLogged");
 
 router.get("/", async (req, res) => {
   const homes = await Home.find({});
   res.render("buy/buy", { homes });
 });
 
-router.get("/new", (req, res) => {
+router.get("/new", isLoggedIn, (req, res) => {
   res.render("buy/newHome");
 });
 
-router.post("/", async (req, res) => {
+router.post("/", isLoggedIn, async (req, res) => {
   const home = new Home(req.body.home);
   await home.save();
+  req.flash("success", "Home added successfully!");
   res.redirect(`buy/${home._id}`);
-});
-
-router.post("/:id/reviews", async (req, res) => {
-  const id = req.params.id;
-  const home = await Home.findById(id);
-  const review = new Review(req.body.review);
-  home.reviews.push(review);
-  await home.save();
-  await review.save();
-  res.redirect(`/buy/${home._id}`);
 });
 
 router.get("/:id", async (req, res) => {
   const id = req.params.id;
   const home = await Home.findById(id).populate("reviews");
+  if (!home) {
+    req.flash("error", "Home not found!");
+    return res.redirect("/buy");
+  }
   res.render("buy/buyDetails", { home });
 });
 
-router.get("/:id/edit", async (req, res) => {
+router.get("/:id/edit", isLoggedIn, async (req, res) => {
   const id = req.params.id;
   const home = await Home.findById(id);
+  if (!home) {
+    req.flash("error", "Home not found!");
+    return res.redirect("/buy");
+  }
   res.render("buy/editHome", { home });
 });
 
-router.put("/:id", async (req, res) => {
+router.put("/:id", isLoggedIn, async (req, res) => {
   const id = req.params.id;
   const home = await Home.findByIdAndUpdate(id, req.body.home);
-  res.redirect(`buy/${home._id}`);
+  req.flash("success", "Successfully updated home!");
+  res.redirect(`/buy/${home._id}`);
 });
 
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", isLoggedIn, async (req, res) => {
   const id = req.params.id;
   await Home.findByIdAndDelete(id);
+  req.flash("success", "Home deleted successfully!");
   res.redirect("/buy");
-});
-
-router.delete("/:id/reviews/:reviewId", async (req, res) => {
-  const { id, reviewId } = req.params;
-  await Home.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
-  await Review.findByIdAndDelete(reviewId);
-  res.redirect(`/buy/${id}`);
 });
 
 // router.get("/search", async (req, res) => {
