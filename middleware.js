4addb4f0c671be85a8b8eea3e85681b94622f3ff
Jerome Haynes <info.devrome@gmail.com>
2022-04-23 20:33:22 -0400

    added isReview middleware

--- middleware.js
+++ middleware.js
@@ -1,3 +1,7 @@
+const Home = require("./models/home-model");
+const Apartment = require("./models/apartment-model");
+const Review = require("./models/review-model");
+
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
     req.session.returnTo = req.originalUrl;
@@ -9,8 +13,8 @@ module.exports.isLoggedIn = (req, res, next) => {
 
 module.exports.isHomeAuthor = async (req, res, next) => {
   const id = req.params.id;
-  const homeAuthor = await Home.findById(id);
-  if (!homeAuthor.author.equals(req.user._id)) {
+  const home = await Home.findById(id);
+  if (!home.user.equals(req.user._id)) {
     req.flash("error", "You are not authorized to do that!");
     return res.redirect(`/buy/${id}`);
   }
@@ -25,4 +29,14 @@ module.exports.isApartmentAuthor = async (req, res, next) => {
     return res.redirect(`/rent/${id}`);
   }
   next();
-}
+};
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+  const { id, reviewId } = req.params;
+  const review = await Review.findById(reviewId);
+  if (!review.user.equals(req.user._id)) {
+    req.flash("error", "You are not authorized to do that!");
+    return res.redirect(`/buy/${id}`);
+  }
+  next();
+};
